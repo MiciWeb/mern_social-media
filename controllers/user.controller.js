dc41c1a383263cdcc79e530109c49aa86c51d618
Mici Srsr <micipsa.sersour@epitech.eu>
2021-11-18 19:36:05 +0100

    add delete users

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -22,17 +22,31 @@ module.exports.updateUser = async (req, res) => {
         return res.status(400).send("ID unknown : " + req.params.id);
     }
 
-        await UserModel.findOneAndUpdate(
-            { _id : req.params.id },
-            {
-                $set: {
-                    bio: req.body.bio
-                }
-            },
-            { new: true, upsert: true, setDefaultsOnInsert: true },
-            (err, docs) => {
-                if (err) return res.status(500).send({ message: err });
-                else return res.send(docs);
+    await UserModel.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+            $set: {
+                bio: req.body.bio
             }
-        )
+        },
+        { new: true, upsert: true, setDefaultsOnInsert: true },
+        (err, docs) => {
+            if (err) return res.status(500).send({ message: err });
+            else return res.send(docs);
+        }
+    )
+}
+
+module.exports.deleteUser = async (req, res) => {
+    if (!ObjectID.isValid(req.params.id)) {
+        return res.status(400).send("ID unknown : " + req.params.id)
+    }
+
+    try {
+        await UserModel.remove({ _id: req.params.id }).exec();
+        res.status(200).json({ message: "Succesfully deleted." });
+    }
+    catch (err) {
+        res.status(500).json({ message: err });
+    }
 }
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -7,5 +7,8 @@ router.post("/register", authController.signUp);
  
 // user DB
 router.get("/", userController.getAllUsers);
+router.get("/:id", userController.userInfo);
+router.put("/:id", userController.updateUser);
+router.delete("/:id", userController.deleteUser);
 
 module.exports = router;
