18ebe05e02bd8a1b26c417cd79a9a50e161a1d1a
Mici Srsr <micipsa.sersour@epitech.eu>
2021-11-23 09:59:51 +0100

    add logout user

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,9 +4,9 @@ const jwt = require('jsonwebtoken');
 const maxAge = 3 * 24 * 60 * 60 * 1000;
 
 const createToken = (id) => {
-  return jwt.sign({id}, process.env.TOKEN_SECRET, {
-    expiresIn: maxAge
-  })
+    return jwt.sign({ id }, process.env.TOKEN_SECRET, {
+        expiresIn: maxAge
+    })
 };
 
 module.exports.signUp = async (req, res) => {
@@ -24,14 +24,19 @@ module.exports.signUp = async (req, res) => {
 
 module.exports.signIn = async (req, res) => {
     const { email, password } = req.body
-  
+
     try {
-      const user = await UserModel.login(email, password);
-      const token = createToken(user._id);
-      res.cookie('jwt', token, { httpOnly: true, maxAge});
-      res.status(200).json({ user: user._id})
-    } catch (err){
-    //   const errors = signInErrors(err);
-    //   res.status(200).json({ errors });
+        const user = await UserModel.login(email, password);
+        const token = createToken(user._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge });
+        res.status(200).json({ user: user._id })
+    } catch (err) {
+        //   const errors = signInErrors(err);
+        res.status(200).json({ err });
     }
-  }
+}
+
+module.exports.logout = async (req,res) => {
+    res.cookie("jwt", "",  {maxAge: 1})
+    res.redirect("/")
+}
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,7 +5,7 @@ const userController = require("../controllers/user.controller");
 // auth
 router.post("/register", authController.signUp);
 router.post("/login", authController.signIn);
-// router.get("/logout", authController.logout);
+router.get("/logout", authController.logout);
 
 // user DB
 router.get("/", userController.getAllUsers);
