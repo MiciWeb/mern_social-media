c1452ab11fa22f8cb0907987e5cd0f0041fc319f
Mici Srsr <micipsa.sersour@epitech.eu>
2021-12-07 11:00:46 +0100

    add add, edit and delete comments

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -73,20 +73,20 @@ module.exports.likePost = async (req, res) => {
                 if (err) return res.status(400).send(err);
                 if (err) return res.status(400).send(err);
                 else
-                UserModel.findByIdAndUpdate(
-                    req.body.id,
-                    {
-                        $addToSet: { likes: req.params.id },
-                    },
-                    { new: true },
-                    (err, docs) => {
-                        if (!err) res.send(docs);
-                        else return res.status(400).send(err);
-                    }
-                );
+                    UserModel.findByIdAndUpdate(
+                        req.body.id,
+                        {
+                            $addToSet: { likes: req.params.id },
+                        },
+                        { new: true },
+                        (err, docs) => {
+                            if (!err) res.send(docs);
+                            else return res.status(400).send(err);
+                        }
+                    );
             }
         );
-        
+
     } catch (err) {
         console.log(err)
     }
@@ -124,3 +124,79 @@ module.exports.unlikePost = async (req, res) => {
         console.log(err)
     }
 }
+
+module.exports.commentPost = (req, res) => {
+    if (!ObjectID.isValid(req.params.id))
+        return res.status(400).send("ID unknown : " + req.params.id);
+
+    try {
+        return PostModel.findByIdAndUpdate(
+            req.params.id,
+            {
+                $push: {
+                    comments: {
+                        commenterId: req.body.commenterId,
+                        commenterPseudo: req.body.commenterPseudo,
+                        text: req.body.text,
+                        timestamp: new Date().getTime(),
+                    },
+                },
+            },
+            { new: true },
+            (err, docs) => {
+                if (!err) return res.send(docs);
+                else return res.status(400).send(err);
+            }
+        );
+    } catch (err) {
+        return res.status(400).send(err);
+    }
+};
+
+
+module.exports.editCommentPost = (req, res) => {
+    if (!ObjectID.isValid(req.params.id))
+        return res.status(400).send("ID unknown : " + req.params.id);
+
+    try {
+        return PostModel.findById(req.params.id, (err, docs) => {
+            const theComment = docs.comments.find((comment) =>
+                comment._id.equals(req.body.commentId)
+            );
+            if (!theComment) return res.status(404).send("Comment not found");
+            theComment.text = req.body.text;
+
+            return docs.save((err) => {
+                if (!err) return res.status(200).send(docs);
+                return res.status(500).send(err);
+            });
+        });
+    } catch (err) {
+        return res.status(400).send(err);
+    }
+};
+
+module.exports.deleteCommentPost = (req, res) => {
+    if (!ObjectID.isValid(req.params.id))
+        return res.status(400).send("ID unknown : " + req.params.id);
+
+    try {
+        return PostModel.findByIdAndUpdate(
+            req.params.id,
+            {
+                $pull: {
+                    comments: {
+                        _id: req.body.commentId,
+                    },
+                },
+            },
+            { new: true },
+            (err, docs) => {
+                if (!err) return res.send(docs);
+                else return res.status(400).send(err);
+            }
+        );
+    } catch (err) {
+        return res.status(400).send(err);
+    }
+};
--- routes/post.routes.js
+++ routes/post.routes.js
@@ -12,9 +12,8 @@ router.delete('/:id', postController.deletePost);
 router.patch('/like-post/:id', postController.likePost);
 router.patch('/unlike-post/:id', postController.unlikePost);
 
-// comments
-// router.patch('/comment-post/:id', postController.commentPost);
-// router.patch('/edit-comment-post/:id', postController.editCommentPost);
-// router.patch('/delete-comment-post/:id', postController.deleteCommentPost);
+router.patch('/comment-post/:id', postController.commentPost);
+router.patch('/edit-comment-post/:id', postController.editCommentPost);
+router.patch('/delete-comment-post/:id', postController.deleteCommentPost);
 
 module.exports = router;
