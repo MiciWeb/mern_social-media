fbe6677a13f2296c58c3cba1f6fc40ce9a69210b
Mici Srsr <micipsa.sersour@epitech.eu>
2021-12-17 17:41:57 +0100

    add avatar to profil and posts

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -13,10 +13,36 @@ module.exports.readPost = (req, res) => {
 }
 
 module.exports.createPost = async (req, res) => {
+    let fileName;
+
+    if (req.file !== null) {
+      try {
+        if (
+          req.file.detectedMimeType != "image/jpg" &&
+          req.file.detectedMimeType != "image/png" &&
+          req.file.detectedMimeType != "image/jpeg"
+        )
+          throw Error("invalid file");
+  
+        if (req.file.size > 500000) throw Error("max size");
+      } catch (err) {
+        const errors = uploadErrors(err);
+        return res.status(201).json({ errors });
+      }
+      fileName = req.body.posterId + Date.now() + ".jpg";
+  
+      await pipeline(
+        req.file.stream,
+        fs.createWriteStream(
+          `${__dirname}/../client/public/uploads/posts/${fileName}`
+        )
+      );
+    }
+
     const newPost = new PostModel({
         posterId: req.body.posterId,
         message: req.body.message,
-        // picture: req.file !== null ? "./uploads/posts/" + fileName : "",
+        picture: req.file !== null ? "./uploads/posts/" + fileName : "",
         video: req.body.video,
         likers: [],
         comments: [],
