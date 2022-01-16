f5616a43b5545268ce269af4bf3687a66baa2ed1
Mici Srsr <micipsa.sersour@epitech.eu>
2022-01-16 22:53:43 +0100

    try to launch heroku again

--- server.js
+++ server.js
@@ -34,6 +34,15 @@ app.get('/jwtid', requireAuth, (req, res) => {
 app.use("/api/user", userRoutes);
 app.use("/api/post", postRoutes);
 
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 // server
 app.listen(process.env.PORT, () => {
     console.log("listening on port " + process.env.PORT);
